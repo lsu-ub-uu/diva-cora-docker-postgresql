46e6c76639313f9bfe0bc888b0d6e010d62fb2d3
perebartrolisimo <pere.bartroli@ub.uu.se>
2021-09-08 11:02:04 +0000

    New import script

--- docker/initdb/importDb.sh
+++ docker/initdb/importDb.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 set -e
 
+function run_sql () {
+   for SQL in "$1"/*
+	do
+ 	 echo "Run file: $SQL"
+  		psql -v ON_ERROR_STOP=1 -U diva diva < $SQL > $SQL.log
+	done
+}
+
+echo ""
 echo "Creating tables"
-TABLE_DIR="sql/tables"
+run_sql "sql/tables"
 
-for TABLE in "$TABLE_DIR"/*
-do
-  echo "$TABLE"
-  psql -v ON_ERROR_STOP=1 -U diva diva < $TABLE > $TABLE.log
-done
-
+echo ""
 echo "Loading data"
-DATA_DIR="/sql/data"
-
-for DATA in "$DATA_DIR"/*
-do
-  echo "$DATA"
-  psql -v ON_ERROR_STOP=1 -U diva diva < $DATA > $DATA.log
-done
-
-
+run_sql "/sql/data"
 
 
 
