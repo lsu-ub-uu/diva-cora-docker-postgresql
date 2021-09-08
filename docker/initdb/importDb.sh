1e604f2dd90a4d5b32e3a4b33a230e15c9264ea2
perebartrolisimo <pere.bartroli@ub.uu.se>
2021-09-08 08:26:27 +0000

    first commit

---
+++ docker/initdb/importDb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+
+#TABLES="organisation organisation_name organisation_address organisation_contact organisation_parent organisation_predecessor organisation_predecessor_description organisation_type organisation_type_name"
+TABLES="person"
+
+for TABLE in $TABLES
+do
+    if [ ! -f /data/$TABLE.imported ]; then
+        psql -v ON_ERROR_STOP=1 -U diva diva < /data/$TABLE.sql > /data/$TABLE.log
+        touch /data/$TABLE.imported
+    fi
+done
